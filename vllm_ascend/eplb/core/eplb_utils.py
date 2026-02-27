[Bugfix.][EEP] Fixed some bugs in EEP and code in the MLP forward process that could lead to precision issues.
Signed-off-by: nifeng <1542305589@qq.com>

vllm_ascend/eplb/core/eplb_utils.py
@@ -89,9 +89,10 @@ def init_eplb_config(eplb_config, layer_id, moe_config):
         global_expert_map.append(expert_map)
         if rankid == moe_config.ep_rank:
             local_expert_map = expert_map.npu()
-    log2phy = generate_log2phy_map(global_expert_map, moe_config.ep_rank).npu() if eplb_enable else None
+    global_expert_map = torch.stack(global_expert_map)
+    log2phy = generate_log2phy_map(global_expert_map)[moe_config.ep_rank].npu() if eplb_enable else None
 
-    return torch.stack(global_expert_map), local_expert_map, log2phy, n_redundant
+    return global_expert_map, local_expert_map, log2phy, n_redundant
 
 
 def reinit_expert(local_expert_map, num_moe_layers, layer_id):
@@ -133,7 +134,7 @@ def generate_log2phy_map(expert_map, sync=False):
     else:
         dice = 0
 
-    log2phy_map = expert_map.clone().cpu()
+    log2phy_map = expert_map.clone().cpu().to(torch.int32)
     device = "cpu"
     world_size, num_experts = log2phy_map.shape
     num_local_experts = log2phy_map.max() + 1

vllm_ascend/eplb/core/eplb_worker.py
@@ -74,6 +74,7 @@ class EplbWorker:
             new_placement = torch.tensor(new_placement)
         self.check_expert_placement(old_placement, new_placement)
         new_expert_maps = self.local2global(new_placement)
+        new_expert_maps_clone = new_expert_maps.clone()
         self.update_expert_map(new_expert_maps)
         self.update_global_placement(new_placement)
 
@@ -90,7 +91,7 @@ class EplbWorker:
                 new_expert_maps = new_expert_maps[:, :old_ep_size]
 
         update_info = self.compose_expert_update_info_greedy(new_expert_maps, self.old_expert_maps)
-        self.old_expert_maps = new_expert_maps.clone()
+        self.old_expert_maps = new_expert_maps_clone
         logger.info("EPLB Process compute complete")
 
         packed_update_info = self.pack_update_info(update_info)
@@ -282,7 +283,17 @@ class EplbWorker:
         log2phy_all = []
         layer_ids = []
 
+        # when scale up, update_info_generator only contains new_expert_map of old ranks, it may cause error in
+        # generate_log2phy_map, so we get new_expert_map from self.shared_dict["expert_map"]
+        scale = self.shared_dict.get("scale", False)
+        scale_up = False
+        if scale:
+            old_ep_size = self.shared_dict["old_ep_size"]
+            new_ep_size = self.shared_dict["new_ep_size"]
+            scale_up = new_ep_size > old_ep_size
         for send_info, recv_info, new_expert_map, layer_id in update_info_generator:
+            if scale and scale_up:
+                new_expert_map = self.shared_dict["expert_maps"][layer_id]
             send_info_this_rank = send_info.get(self.rank_id, [])
             recv_info_this_rank = recv_info.get(self.rank_id, [])
             send_all.append(send_info_this_rank)
@@ -290,7 +301,7 @@ class EplbWorker:
 
             maps.append(new_expert_map[self.rank_id].numpy().tolist())
 
-            log2phy_map = generate_log2phy_map(new_expert_map, self.rank_id)
+            log2phy_map = generate_log2phy_map(new_expert_map)[self.rank_id]
             log2phy_all.append(log2phy_map.numpy().tolist())
 
             layer_ids.append(layer_id)