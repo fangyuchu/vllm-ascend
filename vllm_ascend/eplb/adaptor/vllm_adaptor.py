a70506d8b8214a1463cd8d992e61173154c2c603
nifeng <1542305589@qq.com>
2026-03-28 16:29:35 +0800

    [Feat.][EEP] vLLM Ascend adaptation for Elastic EP Milestone2

--- vllm_ascend/eplb/adaptor/vllm_adaptor.py
+++ vllm_ascend/eplb/adaptor/vllm_adaptor.py
@@ -19,10 +19,10 @@ import json
 from typing import Any
 
 import torch
-import torch.distributed as dist
 from vllm.logger import logger
 
 from vllm_ascend.ascend_config import get_ascend_config
+from vllm_ascend.distributed.parallel_state import get_dynamic_eplb_group
 from vllm_ascend.quantization.methods.base import QuantType
 
 
@@ -46,8 +46,8 @@ class VllmEplbAdaptor:
         else:
             self.model = model
             self.config = model.config
-        self.rank_id = dist.get_rank()
-        self.world_size = dist.get_world_size()
+        self.rank_id = get_dynamic_eplb_group().rank_in_group
+        self.world_size = get_dynamic_eplb_group().world_size
         self.num_dense_layers = getattr(self.config, "first_k_dense_replace", 0)
 
         self.moe_layers = VllmEplbAdaptor._registered_moe_layers
